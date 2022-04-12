42f826fe7a722e76ae58867543ae4951a263c73e
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-04-12 10:35:25 +0200

    a

---
+++ SkysFormsDemo/Infrastructure/Validation/GoodHockeyYearAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkysFormsDemo.Infrastructure.Validation;
+
+public class GoodHockeyYearAttribute : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        int year = int.Parse(value.ToString());
+        var goodyears = new[] { 1953, 1957, 1962, 1987, 1991, 1992, 1998, 2006, 2013 };
+        return goodyears.Contains(year);
+    }
+}
--- SkysFormsDemo/Pages/Register.cshtml.cs
+++ SkysFormsDemo/Pages/Register.cshtml.cs
@@ -1,12 +1,23 @@
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SkysFormsDemo.Data;
+using SkysFormsDemo.Infrastructure.Validation;
 
 namespace SkysFormsDemo.Pages
 {
-    //[BindProperties]
     public class RegisterModel : PageModel
     {
+        private readonly ApplicationDbContext _context;
+
+        [BindProperty]
+        [Required]
+        [Range(1900,2030)]
+        [GoodHockeyYear(ErrorMessage = "Vi vann inte vm då ")]
+        public int Year { get; set; }
+
+
         [BindProperty]
         [Required(ErrorMessage = "Ange en epost tack")]
         [EmailAddress(ErrorMessage = "Ange en epostaddress")]
@@ -33,20 +44,78 @@ namespace SkysFormsDemo.Pages
         [Range(18,50,ErrorMessage = "Ange en ålder mellan 18 och 50")]
         public int Age { get; set; }
 
-//        public string ImportantMessage { get; set; }
+        //public string ImportantMessage { get; set; }
+
+        public List<SelectListItem> Countries { get; set; }
+
+        [BindProperty]
+        [Required]
+        public string CountryCode { get; set; }
+
+
+        [BindProperty]
+        [Required]
+        public string Capital { get; set; }
+
+
+        public RegisterModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
         public void OnGet()
         {
+            FillDropdowns();
         }
+
+        private void FillDropdowns()
+        {
+            Countries = new List<SelectListItem>();
+            Countries.Add(new SelectListItem("Sverige", "SE"));
+            Countries.Add(new SelectListItem("Norge", "NO"));
+            Countries.Add(new SelectListItem("Finland", "FI"));
+        }
+
 
         public IActionResult OnPost()
         {
+
+          
+
+
+            if (CountryCode == "SE")
+            {
+                if (Capital.ToLower() != "stockholm")
+                    ModelState.AddModelError(nameof(Capital),"Ange rätt huvudstad för Sverige");
+            }
+            if (CountryCode == "FI")
+            {
+                if (Capital.ToLower() != "helsingfors")
+                    ModelState.AddModelError(nameof(Capital), "Ange rätt huvudstad för Finland");
+            }
+            if (CountryCode == "NO")
+            {
+                if (Capital.ToLower() != "oslo")
+                    ModelState.AddModelError(nameof(Capital), "Ange rätt huvudstad för oslo");
+            }
+
+            if (ModelState.IsValid)
+            {
+                if (_context.Person.Any(e => e.Email == Email))
+                {
+                    ModelState.AddModelError(nameof(Email),"Du har redan ett konto");
+                }
+            }
+
+
+
             if (ModelState.IsValid)
             {
                 return RedirectToPage("/Index"); //Vi är inloggade
             }
 
-//            ImportantMessage = "Se till att skriva in  rätt dummer";
+            //            ImportantMessage = "Se till att skriva in  rätt dummer";
+            FillDropdowns();
             return Page();
         }
 
