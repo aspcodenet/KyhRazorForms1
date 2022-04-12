3e76834dae5084e90d81b48c3efb4edb24d63df3
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-04-12 09:46:42 +0200

    bindproprty tips

--- SkysFormsDemo/Pages/Register.cshtml.cs
+++ SkysFormsDemo/Pages/Register.cshtml.cs
@@ -4,27 +4,32 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SkysFormsDemo.Pages
 {
-    [BindProperties]
+    //[BindProperties]
     public class RegisterModel : PageModel
     {
+        [BindProperty]
         [Required(ErrorMessage = "Ange en epost tack")]
         [EmailAddress(ErrorMessage = "Ange en epostaddress")]
         public string Email { get; set; }
 
 
+        [BindProperty]
         [Required(ErrorMessage = "sdasdadsa")]
         [Compare(nameof(Email), ErrorMessage = "Ange samma som ovan")]
         public string EmailAgain { get; set; }
 
+        [BindProperty]
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        [BindProperty]
         [Compare(nameof(Password), ErrorMessage = "Ange samma password igen")]
         [DataType(DataType.Password)]
         public string PasswordAgain { get; set; }
 
 
+        [BindProperty]
         [Range(18,50,ErrorMessage = "Ange en ålder mellan 18 och 50")]
         public int Age { get; set; }
 
